e56bb7e335438eca5df01f51eeefeed23f7f35cc
Alexandre Cunha <alexandregomesdacunha@gmail.com>
2024-01-15 08:12:41 -0300

    inital version

---
+++ hd_create_base_dataset.R
@@ -0,0 +1,30 @@
+#------------------------------------------------------------------------------------------------------------------------------
+#This code will:
+#- Create baseline dataset
+#- This dataset will be a panel of individuals by year/baseline year
+#------------------------------------------------------------------------------------------------------------------------------
+
+
+options(file.download.method="wininet")
+
+if (!require("dplyr")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("haven")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("arrow")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("stringr")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("tibble")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("tidyr")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("ggplot2")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("readr")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+if (!require("purrr")) install.packages("splitstackshape", repos="http://artifactory.bcnet.bcb.gov.br/artifactory/cran-remote/")
+
+
+# library(tidyverse)
+# library(haven) 
+# library(arrow)
+
+data_path = "Z:/Bernardus/Cunha_Santos_Doornik/Dta_files"
+output_path = "Z:/Bernardus/Cunha_Santos_Doornik/Output_check"
+scr_path = "Z:/DATA/Dta_files/SCR_TERADATA"
+
+a = Sys.time()
+setwd(data_path)
